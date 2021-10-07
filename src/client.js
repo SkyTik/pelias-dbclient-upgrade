d94d9ced064858ed5cd7065119a0add51ed8257e
vunh <vunh@ahamove.com>
2021-10-07 13:36:29 +0700

    add custom es client settings

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 const elasticsearch = require('@elastic/elasticsearch');
 const settings = require('pelias-config').generate();
 
-module.exports = function () {
-  return new elasticsearch.Client(settings.esclient || {});
+module.exports = function (esClientSettings) {
+  return new elasticsearch.Client(esClientSettings ?? settings.esclient ?? {});
 };
